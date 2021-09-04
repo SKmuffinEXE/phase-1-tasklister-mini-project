c1bb333a65b178326e78d41cd331078004f212ee
David <DEvangelista54@gmail.com>
2021-09-04 15:34:40 -0400

    added task creating

--- src/index.js
+++ src/index.js
@@ -1,3 +1,40 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+
+const taskForm = document.getElementById("create-task-form")
+const taskList = document.getElementById("tasks")
+
+//get response from button
+
+function newTask(){
+  //creation of new list items
+  event.preventDefault()
+  const task = document.createElement("li")
+  task.textContent = event.target.querySelector(`#new-task-description`).value
+
+  //creation of the button
+  const deleteButton = document.createElement("button");
+  deleteButton.className = "delete"
+  deleteButton.textContent = "x"
+  // deleteButton.addEventListener("click",() => task.remove())
+  deleteButton.addEventListener("click",() => deleteTask(task, deleteButton))
+
+  
+  //appending new elements to the task list
+  taskList.append(task, deleteButton)
+}
+
+function deleteTask(task, deleteButton){
+  task.remove()
+  deleteButton.remove()
+
+}
+
+  function init(){
+
+
+  taskForm.addEventListener('submit', newTask)
+  }
+
+  init()
 });
